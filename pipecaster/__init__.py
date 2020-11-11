2d94b429b5e7da6bb157f5cd5a4680655600646a
A. John Callegari <callegari@aya.yale.edu>
2020-11-10 17:33:13 -0800

    cleaning up

--- pipecaster/__init__.py
+++ pipecaster/__init__.py
@@ -1,5 +1,5 @@
 # from pipecaster.pipeline import Pipeline
-from pipecaster.multi_input_pipeline import MultiInputPipeline
+from pipecaster.multi_input_pipeline import Pipeline
 from pipecaster.input_selection import SelectKBestInputs
 
 # from pipecaster.pipes import *
