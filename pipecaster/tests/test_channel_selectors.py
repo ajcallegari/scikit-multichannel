882048f24cb238c6fd51318e455e412b601421a9
A. John Callegari <callegari@aya.yale.edu>
2020-11-25 14:03:33 -0800

    channel selector classification unittests refactored to be the same as the regression tests for consistency

--- pipecaster/tests/test_channel_selectors.py
+++ pipecaster/tests/test_channel_selectors.py
@@ -15,18 +15,11 @@ class TestChannelSelectors(unittest.TestCase):
     ### TEST CHANNEL SELECTORS USING SYNTHETIC CLASSIFICATION DATA ###
 
     @staticmethod
-    def _select_synthetic_classification(channel_selector, n_Xs=20, n_informative_Xs=5, n_weak_Xs=5, weak_noise_sd=10, verbose = 0, seed = None):
+    def _select_synthetic_classification(channel_selector, n_informative_Xs=3, n_weak_Xs=0, n_random_Xs=0, 
+                                         weak_noise_sd=1.0, verbose = 0, seed = None, **sklearn_params):
         
-        n_random_Xs = n_Xs - n_informative_Xs - n_weak_Xs
-        
-        sklearn_params = {'n_classes':2, 
-                          'n_samples':1000, 
-                          'n_features':100, 
-                          'n_informative':20, 
-                          'n_redundant':0, 
-                          'n_repeated':0, 
-                          'class_sep':2.0}
-        
+        n_Xs = n_informative_Xs + n_weak_Xs + n_random_Xs
+
         Xs, y, X_types = synthetic_data.make_multi_input_classification(n_informative_Xs, n_weak_Xs,
                                                          n_random_Xs, weak_noise_sd, seed, **sklearn_params)
 
@@ -58,14 +51,16 @@ class TestChannelSelectors(unittest.TestCase):
         return n_informative_hits, n_random_hits, n_weak_hits
     
     @staticmethod
-    def _test_weak_strong_cls_input_discrimination(channel_selector, n_weak = 5, n_strong = 5, weak_noise_sd = 0.25, seed = None):
-        n_Xs = 2*(n_weak + n_strong)
+    def _test_weak_strong_cls_input_discrimination(channel_selector, n_weak = 5, n_strong = 5, 
+                                                   weak_noise_sd = 0.25, seed = None, **sklearn_params):
+        n_random = n_weak + n_strong
+        n_Xs = n_weak + n_strong + n_random
         n_informative_hits, n_random_hits, n_weak_hits = TestChannelSelectors._select_synthetic_classification(channel_selector, 
-                                                                             n_Xs =  n_Xs,
-                                                                             n_informative_Xs = n_strong,
-                                                                             n_weak_Xs = n_weak, 
-                                                                             weak_noise_sd = weak_noise_sd,
-                                                                             seed = seed)
+                                                                             n_informative_Xs=n_strong,
+                                                                             n_weak_Xs=n_weak, 
+                                                                             n_random_Xs=n_random,
+                                                                             weak_noise_sd=weak_noise_sd,
+                                                                             seed=seed, **sklearn_params)
         passed = True
         if n_informative_hits != n_strong:
             passed = False
@@ -76,14 +71,16 @@ class TestChannelSelectors(unittest.TestCase):
         return passed
     
     @staticmethod
-    def _test_weak_cls_input_detection(channel_selector, n_weak = 5, n_strong = 5, weak_noise_sd = 0.25, seed = None):
-        n_Xs = 2*(n_weak + n_strong)
+    def _test_weak_cls_input_detection(channel_selector, n_weak = 5, n_strong = 5, weak_noise_sd = 0.25, 
+                                       seed = None, **sklearn_params):
+        n_random = n_weak + n_strong
+        n_Xs = n_weak + n_strong + n_random
         n_informative_hits, n_random_hits, n_weak_hits = TestChannelSelectors._select_synthetic_classification(channel_selector, 
-                                                                             n_Xs =  n_Xs,
-                                                                             n_informative_Xs = n_strong,
-                                                                             n_weak_Xs = n_weak, 
-                                                                             weak_noise_sd = weak_noise_sd,
-                                                                             seed = seed)
+                                                                             n_informative_Xs=n_strong,
+                                                                             n_weak_Xs=n_weak, 
+                                                                             n_random_Xs=n_random,
+                                                                             weak_noise_sd=weak_noise_sd,
+                                                                             seed=seed, **sklearn_params)
         passed = True
         if n_informative_hits != n_strong:
             passed = False
@@ -95,18 +92,33 @@ class TestChannelSelectors(unittest.TestCase):
     
     def test_SelectKBestInputs_weak_strong_cls_input_discrimination(self):
         k = 5
+        sklearn_params = {'n_classes':2, 
+                  'n_samples':1000, 
+                  'n_features':100, 
+                  'n_informative':20, 
+                  'n_redundant':0, 
+                  'n_repeated':0, 
+                  'class_sep':2.0}
         channel_selector = SelectKBestInputs(score_func=f_classif, aggregator=np.mean, k=k)
         passed = TestChannelSelectors._test_weak_strong_cls_input_discrimination(channel_selector, n_weak = k, 
-                                                                           n_strong = k, weak_noise_sd = 30, seed = 42)
-        self.assertTrue(passed, 'SelectKBestInputs failed to discriminate between week & strong input matrices')
+                                                                           n_strong = k, weak_noise_sd = 30, 
+                                                                           seed = 42, **sklearn_params)
+        self.assertTrue(passed, 'SelectKBestInputs failed to discriminate between weak & strong classification input matrices')
         
     def test_SelectKBestInputs_weak_cls_input_detection(self):
         k = 10
+        sklearn_params = {'n_classes':2, 
+                  'n_samples':1000, 
+                  'n_features':100, 
+                  'n_informative':20, 
+                  'n_redundant':0, 
+                  'n_repeated':0, 
+                  'class_sep':2.0}
         channel_selector = SelectKBestInputs(score_func=f_classif, aggregator=np.mean, k=k)
         passed = TestChannelSelectors._test_weak_cls_input_detection(channel_selector, n_weak = int(k/2), 
-                                                               n_strong = k - int(k/2), weak_noise_sd = 0.2, seed = 42)
-        self.assertTrue(passed, 'SelectKBestInputs failed to detect all week input matrices') 
-
+                                                               n_strong = k - int(k/2), weak_noise_sd = 0.2, 
+                                                               seed = 42, **sklearn_params)
+        self.assertTrue(passed, 'SelectKBestInputs failed to detect all weak clasification input matrices') 
 
     ### TEST CHANNEL SELECTORS USING SYNTHETIC REGRESSION DATA ###
 
