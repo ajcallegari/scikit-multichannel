613230242ab40be6faf70f7ed43c02890df89b69
A. John Callegari <callegari@aya.yale.edu>
2021-03-25 11:59:39 -0700

    switch from numpydoc to napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1.a1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['numpydoc',
+extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.linkcode']
