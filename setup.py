daa02ad03efaf253db0eb2af42c167f655649b8f
A. John Callegari <callegari@aya.yale.edu>
2021-03-26 14:57:37 -0700

    setup.py edits for PyPI release

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-setup(name='pipecaster',
-      version='0.1',
-      description='test',
-      author='A. John Callegari Jr.',
-      author_email='a.john.callegari@gmail.com',
-      license='MIT',
-      zip_safe=False,
-      packages=find_packages())
+setup(
+    name = 'pipecaster',
+    packages = ['pipecaster'],
+    version = 'v0.1-alpha',  # Ideally should be same as your GitHub release tag varsion
+    description = 'multichannel machine learning library with in-pipeline automation',
+    author = 'A. John Callegari Jr.',
+    author_email = 'a.john.callegari@gmail.com',
+    license='MIT',
+    url = 'https://github.com/ajcallegari/pipecaster/tree/master/pipecaster',
+    download_url = 'https://github.com/ajcallegari/pipecaster/archive/refs/tags/v0.1-alpha.tar.gz',
+    keywords = ['machine learning', 'semi-auto-ml',
+                'multichannel pipeline', 'workflow automation',
+                'ensemble learning', 'machine learning pipeline'],
+    classifiers = [],
+)
