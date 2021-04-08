885d58c21a390f07633c4aaac0acab6fcbdc30b6
A. John Callegari <callegari@aya.yale.edu>
2021-04-07 18:55:58 -0700

    setup.py updated for release v0.3-alpha

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name = 'pipecaster',
     packages = ['pipecaster'],
-    version = 'v0.2-alpha',  # same as your GitHub release tag varsion
+    version = 'v0.3-alpha',  # same as your GitHub release tag varsion
     description = 'multichannel machine learning library with in-pipeline automation',
     long_description = 'Python library for building multichannel machine learning pipelines and for in-pipeline automation (semi-auto-ML)',
     author = 'A. John Callegari Jr.',
     author_email = 'a.john.callegari@gmail.com',
     license='MIT',
     url = 'https://github.com/ajcallegari/pipecaster',
-    download_url = 'https://github.com/ajcallegari/pipecaster/archive/refs/tags/v0.2-alpha.tar.gz',
+    download_url = 'https://github.com/ajcallegari/pipecaster/archive/refs/tags/v0.3-alpha.tar.gz',
     keywords = ['machine learning', 'semi-auto-ml',
                 'multichannel pipeline', 'workflow automation',
                 'ensemble learning', 'machine learning pipeline'],
